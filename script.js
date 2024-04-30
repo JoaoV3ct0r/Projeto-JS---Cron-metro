6c2a621752ec8bc0bc2d7d8e424cbb9cc1385d4e
JoãoV3c0r <jvsantosc02@gmail.com>
2024-04-29 23:48:57 -0300

    Comiit - Atualização no código (Upgrade e inovações)

--- script.js
+++ script.js
@@ -3,65 +3,76 @@ const seg = document.querySelector('.segundos')
 const min = document.querySelector('.minutos')
 const hour = document.querySelector('.horas')
 
+const iniciar = document.querySelector('#iniciar')
+const pausar = document.querySelector('#pausar')
+const continuar = document.querySelector('#continuar')
+const resetar = document.querySelector('#resetar')
+
 let miliNum = 0
 let segNum = 0
 let minNum = 0
 let hourNum = 0
 let INTERVALO
 
-
-function cronometro () {
-    if (miliNum < 99) {
-        miliNum++;
-        if (miliNum < 10) { miliNum = "0" + miliNum }
-        miliseg.innerHTML = "" + miliNum;
-    }
-    if (miliNum == 99) {
-        miliNum = -1;
-    }
-    if (miliNum == 0) {
-        segNum++;
-        if (segNum < 10) { segNum = "0" + segNum }
-        seg.innerHTML = "" + segNum;
-    }
-    if (segNum == 59) {
-        segNum = -1;
-    }
-    if ((miliNum == 0) && (segNum == 0)) {
-        minNum++;
-        if (minNum < 10) { minNum = "0" + minNum}
-        min.innerHTML = "" + minNum;
-    }
-    if (minNum == 59) {
-        minNum = -1;
-    }
-    if ((miliNum == 0) && (segNum == 0) && (minNum == 0) ) {
-        hourNum++;
-        if (hourNum < 10) {hourNum = "0" + hourNum}
-        hour.innerHTML = hourNum;
-    }
-
-}
-
-
 function start() {
     clearInterval(INTERVALO)
     INTERVALO = setInterval(() => {
-        cronometro()
+        if (miliNum < 99) {
+            miliNum++;
+            if (miliNum < 10) { miliNum = "0" + miliNum }
+            miliseg.innerHTML = "" + miliNum;
+        }
+        if (miliNum == 99) {
+            miliNum = -1;
+        }
+        if (miliNum == 0) {
+            segNum++;
+            if (segNum < 10) { segNum = "0" + segNum }
+            seg.innerHTML = "" + segNum;
+        }
+        if (segNum == 59) {
+            segNum = -1;
+        }
+        if ((miliNum == 0) && (segNum == 0)) {
+            minNum++;
+            if (minNum < 10) { minNum = "0" + minNum }
+            min.innerHTML = "" + minNum;
+        }
+        if (minNum == 59) {
+            minNum = -1;
+        }
+        if ((miliNum == 0) && (segNum == 0) && (minNum == 0)) {
+            hourNum++;
+            if (hourNum < 10) { hourNum = "0" + hourNum }
+            hour.innerHTML = hourNum;
+        }
     }, 10)
 
-    // const troca = document.querySelector(".iniciar")
-    // if(start = false){
-    //     troca.innerHTML = "Pausar"
-    //     clearInterval(INTERVALO)
-    // }
+    iniciar.style.display = 'none';
+    pausar.style.display = 'inline-block';
+}
+
+function pause(){
+    clearInterval(INTERVALO)
+
+    pausar.style.display = 'none';
+    continuar.style.display = 'inline-block';
+}
+
+function continua(){
+    start()
+
+    continuar.style.display = 'none';
+    pausar.style.display = 'inline-block';
 }
 
 function reset() {
-    // const iniciar = document.querySelector(".iniciar")
-    // iniciar.innerHTML = "Iniciar"
-    
     clearInterval(INTERVALO)
+
+    iniciar.style.display = 'inline-block';
+    pausar.style.display = 'none';
+    continuar.style.display = 'none';
+
     miliNum = 0
     segNum = 0
     minNum = 0
